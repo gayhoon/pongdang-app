4d76d63e7584d81d3e4ba02c2455490c9ac2c0d7
gayhoon <30451447+gayhoon@users.noreply.github.com>
2025-03-20 19:54:54 +0900

    수정

--- src/app/account/login/page.tsx
+++ src/app/account/login/page.tsx
@@ -21,6 +21,7 @@ const Login = () => {
   // 로그인 시도
   const handleLogin = () => {
     const kakaoAuthURL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_REDIRECT_URI}&response_type=code`;
+    
     window.location.href = kakaoAuthURL;
   };
 
--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -70,17 +70,7 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
       alert("오류 발생. 관리자에게 문의해주세요");
     }
   };
-
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
-      if (isSafari) {
-        console.log("사파리 환경 감지됨! 강제 리로드 실행");
-        window.location.href = window.location.href;
-      }
-    }
-  }, []);
-
+  
   useEffect(() => {
     fetchUser(); // 로그인 상태 초기화
   }, []);
