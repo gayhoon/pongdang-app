6afd0f6b54012879d571a2e134dadbaeabe21aca
gayhoon <30451447+gayhoon@users.noreply.github.com>
2025-03-20 18:43:53 +0900

    외부 이미지 도메인 허용

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: false, // ✅ Strict Mode 비활성화(이게 활성화 되어있으면 개발 모드가 실행되고 이는 useEffect를 두번 실행하는 문제 발생함)
   images: {
+    domains: ["pongdangserver.shop"], // 외부 이미지 도메인 허용
     remotePatterns: [
       {
         // 로컬개발시 [체인지]
